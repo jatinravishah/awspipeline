b11bc6a1e6f82d3938867b27c83566c891def710
Jatin Shah <jatinravishah@gmail.com>
2024-11-11 15:16:48 +0530

    add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the homepage
 @app.route('/')
 def hello_world():
-    return "My name is Jatin"
+    return "My name is Jatin shah"
 
 # Start the server
 if __name__ == '__main__':
